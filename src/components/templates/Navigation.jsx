6f06de6c64857bd9021bb10547593951b15f8494
Beatriz Motta <bigobaldohoran@gmail.com>
2021-08-12 00:20:30 -0300

    Criando Componente Navigation

---
+++ src/components/templates/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <a>Contaot</a>
+            <a>Home</a>
+            <a>Xinxa</a>
+        </div>
+    );
+};
+
+export default Navigation;
