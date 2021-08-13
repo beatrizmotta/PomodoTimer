107cd92e8f713b43f2c967b1e20412103993903e
Beatriz Motta <bigobaldohoran@gmail.com>
2021-08-13 15:51:24 -0300

    Adição de funcionalidade no Componente Navegação

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import Button from './Button';
+import './Navigation.css'
 
-const Navigation = () => {
-    return (
-        <div>
-            <a>Contaot</a>
-            <a>Home</a>
-            <a>Xinxa</a>
-        </div>
-    );
-};
+const initialPage = 'pomodoro'
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props)
+        this.getCurrentPage = this.getCurrentPage.bind(this)
+    }
+    getCurrentPage(e) {
+        this.props.onEvent(e)
+    }
+    render() {
+        return (
+                <div className="navegacao">
+                    <Button onEvent={this.getCurrentPage} timer={'pomodoro'}>Pomodoro</Button>
+                    <Button onEvent={this.getCurrentPage} timer={'shortrest'} >Pausa Rápida</Button>
+                    <Button onEvent={this.getCurrentPage} timer={'longrest'} >Pausa Longa</Button>
+                </div>
+        );
+    }
+}
 
 export default Navigation;
