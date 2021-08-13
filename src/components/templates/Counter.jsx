248d90735f855d536e6846e22a5b1e64bb3524aa
Beatriz Motta <bigobaldohoran@gmail.com>
2021-08-13 15:49:02 -0300

    Componente Counter agora renderiza um timer diferente de acordo com o clique do componente Button da navegação

--- src/components/templates/Counter.jsx
+++ src/components/templates/Counter.jsx
@@ -7,7 +7,32 @@ class Counter extends React.Component {
         super(props)
     }
     render() {
-        return (<Timer {...this.props} />)
+        switch (this.props.name) {
+            case 'pomodoro':
+                return (
+                    <div className="counter">
+                        <Timer className="timer" timer={1500} radius={310} started={false}/>
+                    </div>
+                )
+                break;
+            case 'shortrest':
+                return (
+                    <div className="counter">
+                        <Timer className="timer" timer={300} radius={310} color={"#FF5B3D"} started={false}/>
+                    </div>
+                )
+                break;
+            case 'longrest':
+                return (
+                    <div className="counter">
+                        <Timer className="timer" timer={900} radius={310} color={"#ff8b3d"} started={false}/>
+                    </div>
+                )
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
